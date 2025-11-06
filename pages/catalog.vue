9f104657a21a41b7287853d7d619816cfb66d1a9
jonathan-martz <github@jmartz.de>
2025-11-06 19:18:13 +0100

    Initial commit

---
+++ pages/catalog.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="page bg-white px-3 py-3">
+    <section class="grid grid-cols-8 gap-3">
+      <div class="col-span-4 md:col-span-2">
+        Filter (Name)
+      </div>
+      <div class="col-span-4 md:col-span-2">
+        Filter (Vendor)
+      </div>
+      <div class="col-span-4 md:col-span-2">
+        Filter (Category)
+      </div>
+      <div class="col-span-4 md:col-span-2">
+        Filter (Price) €
+      </div>
+      <div class="col-span-8">
+        <section class="grid grid-cols-6 gap-3">
+          <CatalogProductGrid identifier="bowling" />
+        </section>
+      </div>
+    </section>
+  </section>
+</template>
+<script setup lang="ts">
+import CatalogProductGrid from '@/components/catalog/ProductGrid.vue'
+</script>
