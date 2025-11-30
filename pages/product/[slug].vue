64e9c919ca2be1cb7e4e8629058ad3c6715faf48
jonathan-martz <github@jmartz.de>
2025-11-30 11:01:23 +0100

    update product page, remove product tabs

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -1,66 +1,31 @@
 <template>
-  <section class="page bg-gray-400 px-3 py-3">
-    <section class="grid grid-cols-8 gap-3 px-3 py-3 mx-auto max-w-6xl">
-      <div class="col-span-8 md:col-span-3">
-        <img
-          class="w-full"
-          :src="
-            url +
-            '/api/files/' +
-            item.collectionId +
-            '/' +
-            item.id +
-            '/' +
-            item.cover
-          "
-          alt=""
-        />
-        <ProductGallery :identifier="item.id" />
-      </div>
-      <div class="col-span-8 md:col-span-5">
-        <h2 class="font-bold text-2xl">{{ item.name }}</h2>
-        <p class="text-sm">{{ item.description }}</p>
+  <section class="grid grid-cols-6 gap-3 bg-gray-400 px-3 py-3">
+    <div class="col-span-6 md:col-span-2">
+      <img :src="getMediaUrl(item,'cover')" alt="">
+      <ProductGallery :product="item"/>
+    </div>
+    <div class="col-span-6 md:col-span-4">
+      <h2 class="font-bold text-lg">{{ item.name }}</h2>
+      <p class="text-sm">{{ item.description }}</p>
+      <CustomFields v-if="item.config" :fields="item.config"/>
+      <section class="flex justify-between">
+        <ProductStock :quantity="stock.qty" />
+        <ProductAddToCart :product="item" :qty="stock.qty" :stock="stock"/>
+      </section>
 
-        <review-stars :item="item" />
-        <div class="form-control grid grid-cols-6 gap-3 text-right">
-          <div class="col-span-6">
-            <ProductCustomFields :fields="item.config?.fields" />
-          </div>
-          <div class="col-span-6">
-            <section class="grid grid-cols-6 gap-3">
-              <section class="col-span-6">
-                <ProductStock :quantity="stock.quantity" />
-              </section>
-              <div class="col-span-6 md:col-span-1">
-                <input
-                  type="number"
-                  min="1"
-                  :value="qty"
-                  :max="stock.quantity"
-                  class="input placeholder-black w-full bg-white rounded-lg text-center"
-                />
-              </div>
-              <div class="col-span-6 md:col-span-5">
-                <ProductAddToCart
-                  :stock="stock"
-                  :qty="qty"
-                  :item="item"
-                />
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-      <div class="col-span-8 mt-12">
-        <CatalogProductTabs :identifier="item.id" />
-      </div>
-    </section>
+    </div>
+    <div class="col-span-6">
+      <ProductDetails :identifier="item.id" />
+      <ProductTabs identifier="bowling"/>
+    </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-import { usePocketBase, usePocketBaseUrl } from "~/utils/pocketbase";
-import { useHead } from "@unhead/vue";
+import {usePocketBase, usePocketBaseUrl} from "~/utils/pocketbase";
+import ProductTabs from "~/components/catalog/ProductTabs.vue";
+import CustomFields from "~/components/product/CustomFields.vue";
+import ProductDetails from "~/components/catalog/ProductDetails.vue";
 
 const route = useRoute();
 const pb = usePocketBase();
@@ -70,30 +35,25 @@ const qty = ref(1);
 const stock = ref({});
 
 const load = async () => {
-    item.value = await pb
-        .collection("products")
-        .getFirstListItem(
-            'slug="' + route.params.slug.replace(".html", "") + '"',
-        );
+  item.value = await pb
+      .collection("products")
+      .getFirstListItem(
+          'slug="' + route.params.slug.replace(".html", "") + '"',
+      );
 
-    useHead({
-        title: item.value.name + " - Produkt Ansicht",
-        meta: [
-            {
-                name: "description",
-                content: item.value.description,
-            },
-        ],
-    });
+  useSeoMeta({
+    title: item.value.name + " - Produkt Ansicht",
+    description: item.value.description
+  });
 
-    stock.value = (
-        await pb.collection("product_stocks").getList(1, 1, {
-            filter: 'product="' + item.value.id + '"',
-        })
-    ).items[0];
+  stock.value = (
+      await pb.collection("product_stocks").getList(1, 1, {
+        filter: 'product="' + item.value.id + '"',
+      })
+  ).items[0];
 };
 
 onMounted(() => {
-    load();
+  load();
 });
 </script>
